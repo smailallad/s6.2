c2097511555a799459f0fd6eb11f3103473c3ff7
Smail ALLAD <smailallad@gmail.com>
2024-06-20 12:38:14 +0200

    First commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    #[Route('/', name: 'app_home_')]
+    public function index(): Response
+    {
+        // return $this->render('home/index.html.twig', [
+        //     'controller_name' => 'DefaultController',
+        // ]);
+        return $this->redirectToRoute('app_home');
+    }
+
+    #[Route('/home', name: 'app_home')]
+    public function home(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+}
---
+++ src/Controller/UserController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserController extends AbstractController
+{
+    #[Route('/user/register', name: 'app_user_register')]
+
+    public function register(UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $em)
+    {
+        // ... e.g. get the admin data from a registration form
+        $user = new User();
+        $user->setEmail("admin@gmail.com");
+        $role = ['ROLE_ADMIN'];
+        $user->setRoles($role);
+        $plaintextPassword = "admin";
+
+        // hash the password (based on the security.yaml config for the $user class)
+        $hashedPassword = $passwordHasher->hashPassword(
+            $user,
+            $plaintextPassword
+        );
+        $user->setPassword($hashedPassword);
+        $em->persist($user);
+        //=====
+        $user = new User();
+        $user->setEmail("user@gmail.com");
+        //$role=['ROLE_ADMIN'];
+        //$user->setRoles($role);
+        $plaintextPassword = "user";
+
+        // hash the password (based on the security.yaml config for the $user class)
+        $hashedPassword = $passwordHasher->hashPassword(
+            $user,
+            $plaintextPassword
+        );
+        $user->setPassword($hashedPassword);
+        $em->persist($user);
+        //=====
+        //=====
+        $user = new User();
+        $user->setEmail("super_admin@gmail.com");
+        $role = ['ROLE_SUPER_ADMIN'];
+        $user->setRoles($role);
+        $plaintextPassword = "superadmin";
+
+        // hash the password (based on the security.yaml config for the $user class)
+        $hashedPassword = $passwordHasher->hashPassword(
+            $user,
+            $plaintextPassword
+        );
+        $user->setPassword($hashedPassword);
+        $em->persist($user);
+        //=====
+        $em->flush();
+        return $this->redirectToRoute('app_home');
+
+
+        // ...
+    }
+
+    #[Route('/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('user/login.html.twig', [
+            'controller_name' => 'LoginController',
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ]);
+    }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout()
+    {
+        // controller can be blank: it will never be called!
+        return $this->redirectToRoute('app_home');
+    }
+}
